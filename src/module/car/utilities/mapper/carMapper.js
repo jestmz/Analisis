f3a211952fda14079c33b4d0ed42098f0d72cf59
Lucho <luchoorfino@gmail.com>
2020-10-15 23:36:40 -0300

    Adds Mapper

---
+++ src/module/car/utilities/mapper/carMapper.js
@@ -0,0 +1,8 @@
+const CarEntity = require("../entity/carEntity")
+
+
+module.exports = fromDataToEntity
+
+function fromDataToEntity({ brand, model, year, kms, color, air_conditioning, passengers }) {
+    return new CarEntity({ brand, model, year, kms, color, air_conditioning, passengers })
+}
